38f1fdc8013dc862d4bf4390acce68c11eabfdda
faysalbadaoui <faysal@faysalbadaoui.com>
2024-03-30 16:22:07 +0100

    bug fix

--- app/src/main/java/com/example/tempusky/ui/navigation/TempuskyNavHost.kt
+++ app/src/main/java/com/example/tempusky/ui/navigation/TempuskyNavHost.kt
@@ -7,12 +7,33 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 import com.example.tempusky.domain.appNavigation.NavigationRoutes
+import com.example.tempusky.ui.screens.home.HomeScreen
+import com.example.tempusky.ui.screens.login.LoginScreen
+import com.example.tempusky.ui.screens.profile.ProfileScreen
+import com.example.tempusky.ui.screens.search.SearchScreen
+import com.example.tempusky.ui.screens.settings.SettingsScreen
+import com.example.tempusky.ui.screens.signup.SignupScreen
 
 @Composable
 fun TempuskyNavHost(navController: NavController) {
     NavHost(startDestination = NavigationRoutes.LOGIN, navController = navController as NavHostController) {
         composable(NavigationRoutes.LOGIN){
-
+            LoginScreen()
+        }
+        composable(NavigationRoutes.HOME){
+            HomeScreen()
+        }
+        composable(NavigationRoutes.PROFILE){
+            ProfileScreen()
+        }
+        composable(NavigationRoutes.SETTINGS){
+            SettingsScreen()
+        }
+        composable(NavigationRoutes.SIGNUP){
+            SignupScreen()
+        }
+        composable(NavigationRoutes.SEARCH){
+            SearchScreen()
         }
     }
 }
