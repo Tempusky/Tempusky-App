d4dd91f6f2a75916dd11416a7bc9f3589d2f12ae
Pere <peremunozfiguerol33@gmail.com>
2024-04-01 19:41:31 +0200

    Create object for settings values

---
+++ app/src/main/java/com/example/tempusky/storage/SettingsValues.kt
@@ -0,0 +1,8 @@
+package com.example.tempusky.storage
+
+object SettingsValues {
+    val LANGUAGES = listOf("English", "Spanish")
+    const val DEFAULT_LANGUAGE = "English"
+    val THEMES = listOf("Light", "Dark", "Auto")
+    const val DEFAULT_THEME = "Auto"
+}
