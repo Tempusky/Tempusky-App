58395e62642466c8d133c3156dd29ee1b11c9594
Pere <peremunozfiguerol33@gmail.com>
2024-04-01 19:42:03 +0200

    Create class to manage datastore operations

---
+++ app/src/main/java/com/example/tempusky/storage/SettingsDataStore.kt
@@ -0,0 +1,44 @@
+package com.example.tempusky.storage
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class SettingsDataStore(private val context: Context) {
+
+    companion object {
+        private const val SETTINGS_DATA_STORE_NAME = "settings_data_store"
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = SETTINGS_DATA_STORE_NAME)
+        val LANGUAGE_KEY = stringPreferencesKey("language")
+        val THEME_KEY = stringPreferencesKey("theme")
+    }
+
+    val getLanguage: Flow<String> = context.dataStore.data
+        .map { preferences ->
+            preferences[LANGUAGE_KEY] ?: SettingsValues.DEFAULT_LANGUAGE
+        }
+
+    val getTheme: Flow<String> = context.dataStore.data
+        .map { preferences ->
+            preferences[THEME_KEY] ?: SettingsValues.DEFAULT_THEME
+        }
+
+    suspend fun setLanguage(language: String) {
+        context.dataStore.edit { preferences ->
+            preferences[LANGUAGE_KEY] = language
+        }
+    }
+
+    suspend fun setTheme(theme: String) {
+        context.dataStore.edit { preferences ->
+            preferences[THEME_KEY] = theme
+        }
+    }
+
+
+}
