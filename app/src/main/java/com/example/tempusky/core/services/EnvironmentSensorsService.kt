97796e441367c7bc19db041223d425f3eecb6513
Pere <peremunozfiguerol33@gmail.com>
2024-05-02 17:55:54 +0200

    Create EnvironmentSensorsService that will register the sensors, get values from them and call the receiver to sync the data to firestore

---
+++ app/src/main/java/com/example/tempusky/core/services/EnvironmentSensorsService.kt
@@ -0,0 +1,12 @@
+package com.example.tempusky.core.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class EnvironmentSensorsService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
