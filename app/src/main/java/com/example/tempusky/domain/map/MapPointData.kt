41965eb2590db59c4ab0fd51c1f5b96cda906129
faysalbadaoui <faysal@faysalbadaoui.com>
2024-05-07 16:44:27 +0200

    draw points to display data on map

---
+++ app/src/main/java/com/example/tempusky/domain/map/MapPointData.kt
@@ -0,0 +1,5 @@
+package com.example.tempusky.domain.map
+
+import com.mapbox.geojson.Point
+
+data class MapPointData(val id: String, val point: Point, val title: String, val description: String)
--- app/src/main/java/com/example/tempusky/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/tempusky/ui/screens/home/HomeScreen.kt
@@ -2,11 +2,18 @@ package com.example.tempusky.ui.screens.home
 
 import android.Manifest
 import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.example.tempusky.MainActivity
 import com.example.tempusky.MainViewModel
+import com.example.tempusky.domain.map.MapPointData
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapInitOptions
 import com.mapbox.maps.MapboxExperimental
@@ -27,6 +35,16 @@ import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
 import com.mapbox.maps.extension.style.style
 import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mapbox.maps.ViewAnnotationAnchor
+import com.mapbox.maps.extension.compose.annotation.ViewAnnotation
+import com.mapbox.maps.viewannotation.geometry
+import com.mapbox.maps.viewannotation.viewAnnotationOptions
 
 @OptIn(MapboxExperimental::class)
 @Composable
@@ -35,6 +53,15 @@ fun HomeScreen(context: MainActivity, mainViewModel: MainViewModel) {
     val lightThemeMap = "mapbox://styles/mapbox/outdoors-v12"
     var deviceTheme by remember { mutableStateOf("") }
     var requestingPermissions by remember { mutableStateOf(false) }
+    val locations = listOf(
+        MapPointData("24.5", Point.fromLngLat(0.6206, 41.6148), "Lleida", "1221 Values available"),
+        MapPointData("23", Point.fromLngLat(0.5730, 41.7296), "Albesa", "223 Values available"),
+        MapPointData("22.3", Point.fromLngLat(0.8114, 41.7910), "Balaguer", "852 Values available"),
+        MapPointData("25.8", Point.fromLngLat(0.6430, 41.6755), "Torrefarrera", "156 Values available")
+    )
+    var selectedPoint by remember { mutableStateOf<MapPointData?>(null) }
+
+
     mainViewModel.isLoading.observe(context) {
         requestingPermissions = it
     }
@@ -69,7 +96,7 @@ fun HomeScreen(context: MainActivity, mainViewModel: MainViewModel) {
                 mapViewportState = MapViewportState().apply {
                     setCameraOptions {
                         zoom(11.0)
-                        center(Point.fromLngLat(0.62, 41.6167))
+                        center(Point.fromLngLat(0.6206, 41.6148))
                         pitch(0.0)
                         bearing(0.0)
                     }
@@ -83,9 +110,28 @@ fun HomeScreen(context: MainActivity, mainViewModel: MainViewModel) {
                     .setLocationPuck(createDefault2DPuck(withBearing = false))
                     .setEnabled(true)
                     .build()
-            )
+            ){
+                locations.forEach { location ->
+                    ViewAnnotation(
+                        options = viewAnnotationOptions {
+                            geometry(location.point)
+                            allowOverlap(false)
+                        }
+                    ) {
+                        MapDataObject(data = location)
+                    }
+                }
+            }
         }
 
     }
+}
 
+@Composable
+fun MapDataObject(data: MapPointData){
+    Box(modifier = Modifier
+        .size(40.dp)
+        .background(MaterialTheme.colorScheme.background, RoundedCornerShape(10.dp)).border(2.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(10.dp)), contentAlignment = Alignment.Center) {
+        Text(text = data.id, fontSize = 15.sp, color = MaterialTheme.colorScheme.onBackground, fontWeight = FontWeight.SemiBold)
+    }
 }
